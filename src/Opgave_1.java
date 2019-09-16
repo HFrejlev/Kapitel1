b2ef5fdec8d589f4138368ff16f4625bda9018ce
HFrejlev <henrikfrejlev@hotmail.com>
2019-09-16 16:19:56 +0200

    Opgave fra bog.

---
+++ src/Opgave_1.java
@@ -0,0 +1,28 @@
+public class Opgave_1 {
+
+    public static void main(String[] args) {
+        /*Dette er fælgende opgaver fra bogen i programmering:
+          1.1, 1.2, 1.4, 1.5, 1.6 og 1.12
+        */
+
+        /*1.1: Hardware = fysiske komponenter i en pc som CPU, GPU, RAM (memory) etc.
+               Software = Programmer installeret på en pc som eks. OS, Office, browser etc.
+         */
+
+        /*1.2: CPU (processor), GPU (graphics power unit), RAM (memory), harddrive, PSU
+         */
+
+        // 1.4: Tidligere mHz, men bliver nuværende målt i gHz (omkring 3 gHZ og mere)
+
+        // 1.5 En bit er et tal der enten er 0 (slukket) eller 1 (tændt). En byte er otte bit.
+
+        /* 1.6 Memory bruger den nuværende hukommelse til at indlæse data. Hvis data ikke bliver brugt, bliver de opholdt i harddisken.
+               RAM = random-acces memory - man kan få tilgang til bytes i memory i uvilkårlig rækkefælge (derfor ordet RANDOM)
+        */
+
+        // 1.12 En assembler er et program, der oversætter instruktioner i koden til maskinsprog i bits og bytes (0 og 1).
+
+
+
+    }
+}
